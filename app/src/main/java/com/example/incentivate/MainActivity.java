55d6bee2967e3922cd4d9c96e5a60e761235b651
bri-yan <bri.yan@outlook.com>
2021-01-09 15:36:17 -0800

    added addgoal button

--- app/src/main/java/com/example/incentivate/MainActivity.java
+++ app/src/main/java/com/example/incentivate/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.incentivate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -25,6 +28,14 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+        Button button = findViewById(R.id.goToAddGoal);
+        button.setOnClickListener(this::launchAddGoalActivity);
     }
+
+    public void launchAddGoalActivity(View v) {
+        Intent i = new Intent(this, AddGoalActivity.class);
+        startActivity(i);
+    }
+
 
 }
